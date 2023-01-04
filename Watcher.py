71ebbe928accedf9e52339582fd0574bcddf0c75
TheCuteOwl <114611129+TheCuteOwl@users.noreply.github.com>
2023-01-04 20:36:47 +0100

    Update Watcher.py

--- Watcher.py
+++ Watcher.py
@@ -85,12 +85,9 @@ def flickr(username):
 
 
 def check_vimeo_username(username):
-  # Send a request to the Vimeo API to check the availability of the given username
   api_url = "https://vimeo.com/api/v2/user/check_username.json"
   params = {"username": username}
   response = requests.get(api_url, params=params)
-
-  # If the username is available, the API will return a 404 status code
   if response.status_code == 404:
     return True
   else:
@@ -167,29 +164,75 @@ def spotify(username):
         return False
     return True
 
+def Slack(username):
+    url = f"https://{username}.slack.com/"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return False
+    return True
+
+def Gog(username):
+    url = f"https://www.gog.com/u/{username}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return False
+    return True
+
+def Scratch(username):
+    url = f"https://scratch.mit.edu/users/{username}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return False
+    return True
+
+def Snapchat(username):
+    url = f"https://www.snapchat.com/add/{username}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        return False
+    return True
+
+
+  
+if Snapchat(username):
+  print(Fore.GREEN + f'[+] Snapchat | {username} | is available.')
+else:
+  print(Fore.RED + f'[-] Snapchat | {username} | is not available | https://www.snapchat.com/add/{username}')
+
+if Scratch(username):
+  print(Fore.GREEN + f'[+] Scratch | {username} | is available.')
+else:
+  print(Fore.RED + f'[-] Scratch | {username} | is not available | https://scratch.mit.edu/users/{username}')
+
+if Gog(username):
+  print(Fore.GREEN + f'[+] Gog | {username} | is available.')
+else:
+  print(Fore.RED + f'[-] Gog | {username} | is not available | https://www.gog.com/u/{username}')
+
+if Slack(username):
+  print(Fore.GREEN + f'[+] Slack | {username} | is available.')
+else:
+  print(Fore.RED + f'[-] Slack | {username} | is not available | https://{username}.slack.com/')
+
 if spotify(username):
   print(Fore.GREEN + f'[+] Spotify | {username} | is available.')
 else:
   print(Fore.RED + f'[-] Spotify | {username} | is not available | https://open.spotify.com/user/{username}')
-
 
 if behance(username):
   print(Fore.GREEN + f'[+] Behance | {username} | is available.')
 else:
   print(Fore.RED + f'[-] Behance | {username} | is not available | https://www.behance.net/{username}')
 
-
 if deviantart(username):
   print(Fore.GREEN + f'[+] DeviantArt | {username} | is available.')
 else:
   print(Fore.RED + f'[-] DeviantArt | {username} | is not available | https://www.deviantart.com/{username}')
 
-
 if etsy(username):
   print(Fore.GREEN + f'[+] Etsy | {username} | is available.')
 else:
   print(Fore.RED + f'[-] Etsy | {username} | is not available | https://www.etsy.com/fr/shop/{username}')
-
 
 if soundcloud(username):
   print(Fore.GREEN + f'[+] Soundcloud | {username} | is available.')
@@ -227,7 +270,6 @@ if vimeo:
   print(Fore.GREEN + f"[+] Vimeo | {username} | is available.")
 else:
   print(Fore.RED + f"[-] Vimeo | {username} | is not available. | https://vimeo.com/{username}")
-
 
 if flickr(username):
   print(Fore.GREEN + f"[+] Flickr | {username} | is avaible.")
